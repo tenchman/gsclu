aad4ba033a7b0135ec605b7fa56c4da0ba43423a
Gernot Tenchio <gernot@tenchio.de>
2016-10-26 20:35:15 +0200

    mmapfile: simplified

--- mmapfile.c
+++ mmapfile.c
@@ -5,32 +5,32 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/*
+ * Map the content of a file  in the virtual address space of the calling process.
+ *
+ * @fn - name of the file to open
+ * @st - pointer to a 'struct stat' structure to hold file informations
+**/
 char *mmapfile(char *fn, struct stat *st)
 {
   char *buf = NULL;
   int fd = open(fn, O_RDONLY);
-  int terrno;
+  int __errno;
 
-  if (fd < 0)
-    return NULL;
-
-  if (fstat(fd, st) < 0 || st->st_size <= 0) {
-    terrno = errno;
-    close(fd);
-    errno = terrno;
-    return NULL;
+  if (fd < 0) {
+    /* */
+  } else if (0 > fstat(fd, st)) {
+    /* */
+  } else if ((char *)-1 == (buf = mmap((char *) 0, st->st_size, PROT_READ, MAP_PRIVATE, fd, 0))) {
+    buf = NULL;
   }
 
-  buf = mmap((char *) 0, st->st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-  if (buf == (char *) (-1)) {
-    terrno = errno;
-    close(fd);
-    errno = terrno;
-    return NULL;
-  }
-  /* from mmap(2):
-     "closing the file descriptor does not unmap the region."
-   */
+  /**
+   * from mmap(2):
+   * "closing the file descriptor does not unmap the region."
+  **/
+  __errno = errno;
   close(fd);
+  errno = __errno;
   return buf;
 }
